7b6d4b7dcbba76e201ab1bf81d52f6789ee0bacb
wang_jiawei <wjw202006@126.com>
2026-06-03 20:53:07 +0800

    feat: add login page
    Add a simple static login page with username/password inputs,
    remember me checkbox, and basic styling matching project theme.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/LoginPage.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <LoginPage />
 </template>
+
+<style>
+#app {
+  width: auto;
+  max-width: none;
+  margin: 0;
+  text-align: left;
+  border: none;
+  min-height: auto;
+  display: block;
+}
+</style>
